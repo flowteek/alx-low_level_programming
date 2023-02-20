3ce4ec02c5d2da9dcd90c158b586d3cb104af8ac
samuel <samuelogunbanjo@gmail.com>
2023-02-20 03:09:51 -0800

    my first c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-/*
- *should end with the value (0)
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
