e126e8a503345a7239773cac734c67c984bf206e
samuel <samuelogunbanjo@gmail.com>
2023-02-24 10:02:13 -0800

    print numbers

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ *
+ * Desc: header file
+**/
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void)i;
+void print_numbers(void);
 void print_most_numbers(void);
+
+#endif /* MAIN_H */
