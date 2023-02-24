dbe6e868c6fe4b67c136c831059e33d335cef707
samuel <samuelogunbanjo@gmail.com>
2023-02-24 08:21:48 -0800

    checking for number between 0-9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+include "main.h"
+/**
+ * isadigit - check for a digit 0 through 9
+ * @c: input number
+ * Return: 1 if c is a digit 0 otherwise
+ */ 
+int _isdigit(int c);
+{
+	if (c >= 58 && c <= 67)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
