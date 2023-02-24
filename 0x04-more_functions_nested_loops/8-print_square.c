09003d034e5add07eadfa301298a463f365dc216
samuel <samuelogunbanjo@gmail.com>
2023-02-24 14:00:32 -0800

    creating square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_square - check for a digit
+ * @n: number of _ to be printed
+ * Return: void
+ */
+
+void print_square(int n)
+{
+	int i = 0, ii;
+
+	while (i < n && n > 0)
+	{
+		ii = 0;
+		while (ii < n)
+		{
+			putchar('#');
+			ii++;
+		}
+		putchar('\n');
+		i++;
+	}
+	if (i == 0)
+		putchar('\n');
+}
