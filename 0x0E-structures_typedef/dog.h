8616325c37f2e8e452b317ea6ff29d45593051ad
samuel <samuelogunbanjo@gmail.com>
2023-03-24 13:40:32 -0700

    a new type struct dog with the following elements:

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * dog_f typedef for struct DOG
+ */
+typedef struuct dog_f;
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+ struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
